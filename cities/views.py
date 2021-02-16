36955e74c2b742edde3270257d9f3b1d2ff27e11
SqpuHkS <kovaleksey01@gmail.com>
2021-02-16 09:49:15 +0200

    added detalization with DetailView

--- cities/views.py
+++ cities/views.py
@@ -1,18 +1,19 @@
 from django.shortcuts import render, get_object_or_404
+from django.views.generic import DetailView
 
 from cities.models import City
 
 __all__ = (
-    'home',
+    'home', 'CityDetailView'
 )
 
 
-def home(request, pk=None):
-    if pk:
-        city = get_object_or_404(City, id=pk)
-
-        context = {'object': city}
-        return render(request, 'cities/detail.html', context)
+def home(request):
     qs = City.objects.all()
     context = {'objects_list': qs}
     return render(request, 'cities/home.html', context)
+
+class CityDetailView(DetailView):
+    queryset = City.objects.all()
+    template_name = 'cities/detail.html'
+
