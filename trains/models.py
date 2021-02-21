3a0efb838a46cfbeeb60c3529fea826a02191a90
SqpuHkS <kovaleksey01@gmail.com>
2021-02-21 12:14:02 +0200

    added train model and added trains app to the settings

--- trains/models.py
+++ trains/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+from cities.models import City
+
+
+class Train(models.Model):
+    name = models.CharField(max_length=50, unique=True, verbose_name='Train name')
+    travel_time = models.PositiveSmallIntegerField(verbose_name='Travel time')
+    from_city = models.ForeignKey(City, on_delete=models.CASCADE, related_name='from_city_set', verbose_name='From which city')
+    to_city = models.ForeignKey(City, on_delete=models.CASCADE, related_name='to_city_set', verbose_name='To which city')
+
+    def __str__(self):
+        return f'Train №{self.name} from city {self.from_city}'
+
+    class Meta:
+        verbose_name_plural = 'Trains'
+        ordering = ['travel_time']
