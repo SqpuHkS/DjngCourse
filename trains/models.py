c1e44d3c28c16323cb15895e3eebc414fa49453e
SqpuHkS <kovaleksey01@gmail.com>
2021-02-22 11:08:48 +0200

    added class Train to the admin panel and redefined the clean and save method for check errors

--- trains/models.py
+++ trains/models.py
@@ -1,3 +1,4 @@
+from django.core.exceptions import ValidationError
 from django.db import models
 
 from cities.models import City
@@ -6,8 +7,8 @@ from cities.models import City
 class Train(models.Model):
     name = models.CharField(max_length=50, unique=True, verbose_name='Train name')
     travel_time = models.PositiveSmallIntegerField(verbose_name='Travel time')
-    from_city = models.ForeignKey(City, on_delete=models.CASCADE, related_name='from_city_set', verbose_name='From which city')
-    to_city = models.ForeignKey(City, on_delete=models.CASCADE, related_name='to_city_set', verbose_name='To which city')
+    from_city = models.ForeignKey(City, on_delete=models.CASCADE, related_name='from_city_set', verbose_name='From city')
+    to_city = models.ForeignKey(City, on_delete=models.CASCADE, related_name='to_city_set', verbose_name='To city')
 
     def __str__(self):
         return f'Train №{self.name} from city {self.from_city}'
@@ -15,3 +16,18 @@ class Train(models.Model):
     class Meta:
         verbose_name_plural = 'Trains'
         ordering = ['travel_time']
+
+    def clean(self, *args, **kwargs):
+        if self.from_city == self.to_city:
+            raise ValidationError('Change the city from')
+        qs = Train.objects.filter(
+            from_city=self.from_city,
+            to_city=self.to_city,
+            travel_time=self.travel_time,
+        ).exclude(pk=self.pk)
+        if qs.exists():
+            raise ValidationError('Change the travel time')
+
+        def save(self, *args, **kwargs):
+            self.clean()
+            super().save(*args, **kwargs)
