5a6798ad4c811caa8328430ba6d044d206549895
SqpuHkS <kovaleksey01@gmail.com>
2021-02-27 14:44:46 +0200

    added routes home view function, template and urls in main urls and in routes

--- find_route/urls.py
+++ find_route/urls.py
@@ -18,9 +18,11 @@ from django.urls import path, include
 
 from cities.urls import *
 from trains.urls import *
+from routes.urls import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('cities/', include(('cities.urls', 'cities'))),
     path('trains/', include(('trains.urls', 'trains'))),
+    path('routes/', include(('routes.urls', 'routes'))),
 ]
