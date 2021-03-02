d58cc50ea1fb54321f807702f1c2b45ed1a7e640
SqpuHkS <kovaleksey01@gmail.com>
2021-03-02 14:45:21 +0200

    added new function for creating graph

--- routes/utils.py
+++ routes/utils.py
@@ -1,4 +1,11 @@
 
 
+def get_graph(qs):
+    graph = {}
+    for q in qs:
+        graph.setdefault(q.from_city_id, set())
+        graph[q.from_city_id].add(q.to_city_id)
+    return graph
+
 def get_routes(request, form) -> dict:
     pass
