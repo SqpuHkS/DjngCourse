04f3acceadd9613e73d3cae34754e6df0da25b31
SqpuHkS <kovaleksey01@gmail.com>
2021-03-12 11:00:08 +0200

    added list of all users routes

--- routes/urls.py
+++ routes/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('find_routes/', find_routes, name='find_routes'),
     path('add_route/', add_route, name='add_route'),
     path('save_route/', save_route, name='save_route'),
+    path('list/', RouteListView.as_view(), name='list_route'),
     path('', home, name='home'),
 ]
