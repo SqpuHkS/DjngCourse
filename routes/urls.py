97d15d7e7c2a6da70a019d8ba0231958dbbc9494
SqpuHkS <kovaleksey01@gmail.com>
2021-03-15 11:05:31 +0200

    facilitated names of routes urls and added detail page for the choosen route

--- routes/urls.py
+++ routes/urls.py
@@ -3,9 +3,10 @@ from django.urls import path
 from routes.views import *
 
 urlpatterns = [
-    path('find_routes/', find_routes, name='find_routes'),
-    path('add_route/', add_route, name='add_route'),
-    path('save_route/', save_route, name='save_route'),
-    path('list/', RouteListView.as_view(), name='list_route'),
+    path('find_routes/', find_routes, name='find'),
+    path('add_route/', add_route, name='add'),
+    path('save_route/', save_route, name='save'),
+    path('list/', RouteListView.as_view(), name='list'),
+    path('detail/<int:pk>/', RouteDetailView.as_view(), name='detail'),
     path('', home, name='home'),
 ]
