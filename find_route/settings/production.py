fa4b00b072b0f78032559bfa500a7aa0f9cdf535
SqpuHkS <kovaleksey01@gmail.com>
2021-03-18 10:32:16 +0200

    added libs to work with databases

--- find_route/settings/production.py
+++ find_route/settings/production.py
@@ -87,11 +87,18 @@ WSGI_APPLICATION = 'find_route.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': DB_NAME,
+        'USER': DB_USER,
+        'PASSWORD': DB_PASSWORD,
+        'HOST': DB_HOST,
+        'PORT': '5432'
     }
 }
 
+import dj_database_url
+db = dj_database_url.config()
+DATABASES['default'].update(db)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
